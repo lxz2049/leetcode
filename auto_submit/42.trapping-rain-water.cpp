9b7e638e5afe7e4cf479b7440401d2ac1439bef4
lxz2049 <lxz2049@gmail.com>
2018-01-26 08:46:04 -0800

    update 42.trapping-rain-water.cpp

--- auto_submit/42.trapping-rain-water.cpp
+++ auto_submit/42.trapping-rain-water.cpp
@@ -1,49 +1,63 @@
+/*
+ * [42] Trapping Rain Water
+ *
+ * https://leetcode.com/problems/trapping-rain-water/description/
+ *
+ * algorithms
+ * Hard (37.43%)
+ * Total Accepted:    148K
+ * Total Submissions: 395.5K
+ * Testcase Example:  '[]'
+ *
+ * 
+ * Given n non-negative integers representing an elevation map where the width
+ * of each bar is 1, compute how much water it is able to trap after
+ * raining. 
+ * 
+ * 
+ * 
+ * For example, 
+ * Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+ * 
+ * 
+ * 
+ * 
+ * The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
+ * In this case, 6 units of rain water (blue section) are being trapped. Thanks
+ * Marcos for contributing this image!
+ */
 class Solution {
 public:
     int trap(vector<int>& height) {
-		vector<int> new_height(height);
+		// create a copy of height
 		int size = height.size();
-		for (int i = 1; i < size-1; ++i) {
-			if (left(new_height, i) && right(new_height, i)) {
-				//printf("found low point %d\n", i);
-				int l=i;
-				int r=i;
-				while (left(new_height, l)) {
-					l--;
-				}
-				while (right(new_height, r)) {
-					r++;
-				}
-				//printf("expanded left to %d\n", l);
-				//printf("expanded right to %d\n", r);
-				int low = new_height[i];
-				int high = new_height[l] < new_height[r] ? new_height[l] : new_height[r];
-				for (int j = l+1; j < r; j++) {
-					new_height[j] = new_height[j] < high ? high : new_height[j];
-				}
-			}
+		vector<int>water(height);
+
+		// store lmax
+		int lmax=0;
+		for (int i=0; i<size-1; ++i) {
+			if (height[i] > lmax)
+				lmax = height[i];
+			water[i] = lmax;
 		}
 
-		// debug
-		for (int i = 1; i < size-1; ++i) {
-			//printf("%d ", new_height[i]);
+		// store rmax
+		int rmax = 0;
+		for (int i=size-1; i>0; --i) {
+			if (height[i] > rmax)
+				rmax = height[i];
+			water[i] = min(rmax, water[i]);
+		}
+
+		// get result
+		int ans=0;
+		for (int i=0; i<size; ++i) {
+			//printf("%d ", water[i]);
+			ans += water[i] - height[i];
 		}
 		//printf("\n");
-		// debug
 
-		int volumn = 0;
-		for (int i = 1; i < size-1; ++i) {
-			volumn += new_height[i] - height[i] > 0 ? new_height[i] - height[i] : 0;
-		}
-		return volumn;
+		return ans;
     }
-
-	bool left(vector<int>& height, int l) {
-		return height[l-1] >= height[l] && l-1 >= 0;
-	}
-
-	bool right(vector<int>& height, int r) {
-		return height[r+1] >= height[r] && r+1 < height.size();
-	}
-
 };
+
