8db3a81424a9998836eccef25720abf7d5e984c4
lxz2049 <lxz2049@gmail.com>
2018-01-26 09:01:20 -0800

    test harness

---
+++ test/leetcode.h
@@ -0,0 +1,7 @@
+#include <vector>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <vector>
 
-int main()
-{
-	printf("hello world!\n");
+#include "42.trapping-rain-water.cpp"
+
+using namespace std;
+
+int main() {
+	Solution s = Solution();
+	int nums_array[12] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+	//int nums_array[6] = {5, 2, 1, 2, 1, 5};
+	vector<int> nums(nums_array, nums_array + sizeof(nums_array) / sizeof(int));
+	printf("%d\n", s.trap(nums));
+
 	return 0;
 }
