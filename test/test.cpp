e50bb42c0bf4c1fde1ed0343e16f5398b3679f74
lxz2049 <lxz2049@gmail.com>
2018-01-29 08:29:37 -0800

    update test script

--- test/test.cpp
+++ test/test.cpp
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <vector>
 
-#include "42.trapping-rain-water.cpp"
-
 using namespace std;
 
 int main() {
 	Solution s = Solution();
-	int nums_array[12] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-	//int nums_array[6] = {5, 2, 1, 2, 1, 5};
-	vector<int> nums(nums_array, nums_array + sizeof(nums_array) / sizeof(int));
-	printf("%d\n", s.trap(nums));
-
+	if (s.test())
+		cout<<"AC"<<endl;
+	else
+		cout<<"WRONG ANSWER"<<endl;
 	return 0;
 }
