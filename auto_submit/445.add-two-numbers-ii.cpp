e76c7f40a23f86450f34b6f11be7895dd807f2ae
lxz2049 <lxz2049@gmail.com>
2018-02-12 13:12:11 -0800

    update 445.add-two-numbers-ii.cpp

---
+++ auto_submit/445.add-two-numbers-ii.cpp
@@ -0,0 +1,83 @@
+/*
+ * [445] Add Two Numbers II
+ *
+ * https://leetcode.com/problems/add-two-numbers-ii/description/
+ *
+ * algorithms
+ * Medium (46.08%)
+ * Total Accepted:    42.9K
+ * Total Submissions: 93.1K
+ * Testcase Example:  '[7,2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The most significant digit comes first and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Follow up:
+ * What if you cannot modify the input lists? In other words, reversing the
+ * lists is not allowed.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 8 -> 0 -> 7
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // reverse two lists
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        // add two lists
+        ListNode *prev = NULL;
+        int val = 0;
+        while (l1 || l2) {
+            if (l1) {
+                val += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                val += l2->val;
+                l2 = l2->next;
+            }
+            ListNode* n = new ListNode(val%10);
+            n->next = prev;
+            prev = n;
+            val /= 10;
+        }
+        if (val) {
+            ListNode* n = new ListNode(val%10);
+            n->next = prev;
+            prev = n;
+        }
+
+        return prev;
+    }
+
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        while (head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+};
