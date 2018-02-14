befbd7462392c75b602d63a4c35d94d923c6bea7
Evan Li (Visual Concepts) <eli@2kgnoveli.2kgames.t2.corp>
2018-02-14 12:18:14 -0800

    add new test.sh harness

---
+++ test/test.sh
@@ -0,0 +1,8 @@
+if [[ $1 =~ \.cpp$ ]]
+then
+    g++ --include leetcode.h --include $1 test.cpp -o test
+fi
+if [[ $1 =~ \.py$ ]]
+then
+    python -c "import imp; print imp.load_source('', '$1').Solution().test()"
+fi
