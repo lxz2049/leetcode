d5fcbaf9fa37c3b4d07d0c22a80d0cb93e93e7a2
Evan Li (Visual Concepts) <eli@2kgnoveli.2kgames.t2.corp>
2018-02-21 16:23:14 -0800

    update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+echo "creating temp dir"
+TMPDIR=$(mktemp -d)
+trap "echo removing temp dir; rm -rf $TMPDIR" EXIT
+
+echo "converting source to ascii"
+iconv -f utf-8 -t ascii//TRANSLIT < $1 > $TMPDIR/tmp
+cp $TMPDIR/tmp $1
+
 if [[ $1 =~ \.cpp$ ]]
 then
-    g++ --include leetcode.h --include $1 test.cpp -o test
-    ./test
+    echo "compiling $1"
+    g++ --include leetcode.h --include $1 test.cpp -o $TMPDIR/_test
+    TMPDIR/_test
 fi
 if [[ $1 =~ \.py$ ]]
 then
-    python -c "import imp; print imp.load_source('', '$1').Solution().test()"
+    echo "interpreting $1"
+    python -c "import imp; imp.load_source('', '$1').Solution().test()"
 fi
