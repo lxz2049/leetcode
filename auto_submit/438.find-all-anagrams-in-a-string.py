de763cea4e6e26edbee4c09a8621a98889163507
lxz2049 <lxz2049@gmail.com>
2018-02-25 18:54:45 -0800

    update 438.find-all-anagrams-in-a-string.py

---
+++ auto_submit/438.find-all-anagrams-in-a-string.py
@@ -0,0 +1,73 @@
+#
+# [438] Find All Anagrams in a String
+#
+# https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
+#
+# algorithms
+# Easy (33.97%)
+# Total Accepted:    56.6K
+# Total Submissions: 166.7K
+# Testcase Example:  '"cbaebabacd"\n"abc"'
+#
+# Given a string s and a non-empty string p, find all the start indices of p's
+# anagrams in s.
+# 
+# Strings consists of lowercase English letters only and the length of both
+# strings s and p will not be larger than 20,100.
+# 
+# The order of output does not matter.
+# 
+# Example 1:
+# 
+# Input:
+# s: "cbaebabacd" p: "abc"
+# 
+# Output:
+# [0, 6]
+# 
+# Explanation:
+# The substring with start index = 0 is "cba", which is an anagram of "abc".
+# The substring with start index = 6 is "bac", which is an anagram of "abc".
+# 
+# 
+# 
+# Example 2:
+# 
+# Input:
+# s: "abab" p: "ab"
+# 
+# Output:
+# [0, 1, 2]
+# 
+# Explanation:
+# The substring with start index = 0 is "ab", which is an anagram of "ab".
+# The substring with start index = 1 is "ba", which is an anagram of "ab".
+# The substring with start index = 2 is "ab", which is an anagram of "ab".
+# 
+# 
+#
+import collections
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        pcounter = collections.Counter(p)
+        scounter = collections.Counter()
+        start = 0
+        ans = []
+        for i, c in enumerate(s):
+            scounter[c] += 1
+            #print c, s[start], scounter
+            while scounter[c] > pcounter[c] and start <= i:
+                scounter[s[start]] -= 1
+                start += 1
+            if i-start+1 == len(p):
+                ans.append(start)
+        return ans
+
+    def test(self):
+        print self.findAnagrams("cbaebabacd", "abc")
+        print self.findAnagrams("abab", "ab")
