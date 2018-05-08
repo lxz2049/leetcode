4c9be0593facd0143b5c597f455cca4806b91f98
Evan Li (Visual Concepts) <eli@2kgnoveli.2kgames.t2.corp>
2018-05-07 17:16:18 -0700

    new helper function to build binary tree

--- test/leetcode.h
+++ test/leetcode.h
@@ -16,6 +16,21 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+TreeNode* arr2treeUtil(int x[], int i, int len) {
+    if (i >= len)   return NULL;
+    TreeNode* node = new TreeNode(x[i]);
+    node->left = arr2treeUtil(x, i*2+1, len);
+    node->right = arr2treeUtil(x, i*2+2, len);
+    return node;
+}
+
+TreeNode* arr2tree(int x[], int len) {
+    TreeNode* root = new TreeNode(x[0]);
+    root->left = arr2treeUtil(x, 1, len);
+    root->right = arr2treeUtil(x, 2, len);
+    return root;
+}
+
 struct ListNode {
 	int val;
 	ListNode* next;
@@ -33,7 +48,7 @@ struct ListNode {
 ListNode* arr2list(int x[], int len) {
     ListNode* head = new ListNode(x[0]);
     ListNode* node = head;
-    for (int i=0; i<len; ++i) {
+    for (int i=1; i<len; ++i) {
         node = node->next = new ListNode(x[i]);
     }
     return head;
