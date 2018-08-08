0c3c1124d035d1a429caf7a4e91d38a3840ee1a8
lxz2049 <lxz2049@gmail.com>
2018-08-08 12:28:57 -0700

    update 643.maximum-average-subarray-i.py

--- auto_submit/643.maximum-average-subarray-i.py
+++ auto_submit/643.maximum-average-subarray-i.py
@@ -37,17 +37,10 @@ class Solution(object):
         :type k: int
         :rtype: float
         """
-        max_sum = -10000*k
-        cur_sum = 0.0
-        for i, n in enumerate(nums):
-            if i >= k:
-                cur_sum = cur_sum - nums[i-k] + n
-                max_sum = max(cur_sum, max_sum)
-            else:
-                cur_sum += n
-                if i == k-1:
-                    max_sum = max(cur_sum, max_sum)
-            #print cur_sum
+        cur_sum = max_sum = sum(nums[:k])
+        for i in xrange(k, len(nums)):
+            cur_sum = cur_sum - nums[i-k] + nums[i]
+            max_sum = max(cur_sum, max_sum)
         return float(max_sum)/k
 
     def test(self):
