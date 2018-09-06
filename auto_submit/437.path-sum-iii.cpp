c239310e99f35131ef1e0998223765ed530933a3
lxz2049 <lxz2049@gmail.com>
2018-09-06 00:02:17 -0700

    update 437.path-sum-iii.cpp

--- auto_submit/437.path-sum-iii.cpp
+++ auto_submit/437.path-sum-iii.cpp
@@ -6,8 +6,7 @@ public:
             sum += node->val;
             ret += prefixSum[sum-target];
             prefixSum[sum]++;
-            ret +=  calPathSum(node->left, prefixSum, target, sum) + calPathSum(node->right, prefixSum, target, sum);
-            //cout<<sum-target<<" "<<prefixSum[sum-target]<<endl;
+            ret += calPathSum(node->left, prefixSum, target, sum) + calPathSum(node->right, prefixSum, target, sum);
             prefixSum[sum]--;
         }
         return ret;
@@ -17,15 +16,5 @@ public:
         unordered_map<int, int> prefixSum;
         prefixSum[0] = 1;
         return calPathSum(root, prefixSum, target, 0);
-    }
-
-    void test() {
-        //int a[] = {5,4,8,11,-1,13,4,7,2,-1,-1,5,1};
-        //TreeNode* root = arr2tree(a, sizeof(a) / sizeof(int));
-        //cout<<3<<" ? "<<pathSum(root, 22)<<endl;
-        
-        //int a[] = {1};
-        //TreeNode* root = arr2tree(a, sizeof(a) / sizeof(int));
-        //cout<<0<<" ? "<<pathSum(root, 0)<<endl;
     }
 };
