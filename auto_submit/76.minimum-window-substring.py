2a08f40150e40926c5ea358976c77cc5e8004269
lxz2049 <lxz2049@gmail.com>
2018-09-26 00:19:10 -0700

    update 76.minimum-window-substring.py

--- auto_submit/76.minimum-window-substring.py
+++ auto_submit/76.minimum-window-substring.py
@@ -37,30 +37,25 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        seen = set()
-        tcounter = Counter(t)
-        counter = Counter()
+        counter = Counter(t)
+        count = len(counter)
         j = 0
         ret_i = None
         ret_j = None
         for i, c in enumerate(s):
-            if c in tcounter:
-                counter[c] += 1
-                if counter[c] == tcounter[c]:
-                    seen.add(c)
-                #print s[j], counter[s[j]]
-                while s[j] not in tcounter or counter[s[j]] > tcounter[s[j]]:
-                    if s[j] in counter:
-                        counter[s[j]] -= 1
-                    j += 1
-                if len(seen) == len(tcounter):
-                    #print s[j:i+1], ret_j, ret_i, j, i
-                    if ret_i is None or ret_i - ret_j > i - j:
-                        ret_i = i
-                        ret_j = j
+            counter[c] -= 1
+            if counter[c] == 0:
+                count -= 1
+            while j < i and counter[s[j]] < 0:
+                counter[s[j]] += 1
+                j += 1
+            if count == 0:
+                if ret_i is None or ret_i - ret_j > i - j:
+                    ret_i = i
+                    ret_j = j
 
         return s[ret_j:ret_i+1] if ret_j is not None else ""
 
     def test(self):
         print self.minWindow("ADOBECODEBANC", "ABC")
-                    
+        print self.minWindow("aa", "aa")
