0a7327cf4e751a9fd5454bd61937c0d194387de8
lxz2049 <lxz2049@gmail.com>
2018-09-30 23:58:00 -0700

    update 599.minimum-index-sum-of-two-lists.py

---
+++ auto_submit/599.minimum-index-sum-of-two-lists.py
@@ -0,0 +1,72 @@
+#
+# [599] Minimum Index Sum of Two Lists
+#
+# https://leetcode.com/problems/minimum-index-sum-of-two-lists/description/
+#
+# algorithms
+# Easy (46.51%)
+# Total Accepted:    40.7K
+# Total Submissions: 87.6K
+# Testcase Example:  '["Shogun","Tapioca Express","Burger King","KFC"]\n["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]'
+#
+# 
+# Suppose Andy and Doris want to choose a restaurant for dinner, and they both
+# have a list of favorite restaurants represented by strings. 
+# 
+# 
+# You need to help them find out their common interest with the least list
+# index sum. If there is a choice tie between answers, output all of them with
+# no order requirement. You could assume there always exists an answer.
+# 
+# 
+# 
+# Example 1:
+# 
+# Input:
+# ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+# ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+# Output: ["Shogun"]
+# Explanation: The only restaurant they both like is "Shogun".
+# 
+# 
+# 
+# Example 2:
+# 
+# Input:
+# ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+# ["KFC", "Shogun", "Burger King"]
+# Output: ["Shogun"]
+# Explanation: The restaurant they both like and have the least index sum is
+# "Shogun" with index sum 1 (0+1).
+# 
+# 
+# 
+# 
+# Note:
+# 
+# The length of both lists will be in the range of [1, 1000].
+# The length of strings in both lists will be in the range of [1, 30].
+# The index is starting from 0 to the list length minus 1.
+# No duplicates in both lists.
+# 
+# 
+#
+class Solution(object):
+    def findRestaurant(self, list1, list2):
+        """
+        :type list1: List[str]
+        :type list2: List[str]
+        :rtype: List[str]
+        """
+        ret = []
+        isum = len(list1) + len(list2)
+        s = {r:i for i, r in enumerate(list1)}
+        for i, r in enumerate(list2):
+            if r in s:
+                if s[r] + i < isum:
+                    ret = [r]
+                    isum = s[r] + i
+                elif s[r] + i == isum:
+                    ret.append(r)
+        return ret
+                
