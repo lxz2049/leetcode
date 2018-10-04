59117a046e5b5c3646f0a39b01b3f808f7093247
lxz2049 <lxz2049@gmail.com>
2018-10-03 22:20:09 -0700

    update 768.max-chunks-to-make-sorted-ii.cpp

---
+++ auto_submit/768.max-chunks-to-make-sorted-ii.cpp
@@ -0,0 +1,81 @@
+/*
+ * [779] Max Chunks To Make Sorted II
+ *
+ * https://leetcode.com/problems/max-chunks-to-make-sorted-ii/description/
+ *
+ * algorithms
+ * Hard (43.64%)
+ * Total Accepted:    7.3K
+ * Total Submissions: 16.6K
+ * Testcase Example:  '[5,4,3,2,1]'
+ *
+ * This question is the same as "Max Chunks to Make Sorted" except the integers
+ * of the given array are not necessarily distinct, the input array could be up
+ * to length 2000, and the elements could be up to 10**8.
+ * 
+ * 
+ * 
+ * Given an array arr of integers (not necessarily distinct), we split the
+ * array into some number of "chunks" (partitions), and individually sort each
+ * chunk.  After concatenating them, the result equals the sorted array.
+ * 
+ * What is the most number of chunks we could have made?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: arr = [5,4,3,2,1]
+ * Output: 1
+ * Explanation:
+ * Splitting into two or more chunks will not return the required result.
+ * For example, splitting into [5, 4], [3, 2, 1] will result in [4, 5, 1, 2,
+ * 3], which isn't sorted.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: arr = [2,1,3,4,4]
+ * Output: 4
+ * Explanation:
+ * We can split into two chunks, such as [2, 1], [3, 4, 4].
+ * However, splitting into [2, 1], [3], [4], [4] is the highest number of
+ * chunks possible.
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * arr will have length in range [1, 2000].
+ * arr[i] will be an integer in range [0, 10**8].
+ * 
+ * 
+ * 
+ */
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        vector<int> maxarr(arr);
+        vector<int> minarr(arr);
+        for (int i=arr.size()-2; i>=0; --i) {
+            maxarr[i] = max(maxarr[i+1], maxarr[i]);
+            minarr[i] = min(minarr[i+1], minarr[i]);
+        }
+
+        int ret = 1;
+        int amax = 0, amin = 0x7fffffff;
+        for (int i=0; i<arr.size()-1; ++i) {
+            amax = max(arr[i], amax);
+            if (amax <= minarr[i+1]) {
+                ret ++;
+                amax = 0;
+            }
+        }
+        return ret;
+    }
+
+    void test() {
+        vector<int> arr = {2,1,3,4,4};
+        cout<<maxChunksToSorted(arr)<<endl;
+    }
+};
