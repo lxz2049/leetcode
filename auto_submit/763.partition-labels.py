5b7cf209ae15e421994deed9e96a7d32a90b3bcd
lxz2049 <lxz2049@gmail.com>
2018-10-12 22:27:54 -0700

    update 763.partition-labels.py

--- auto_submit/763.partition-labels.py
+++ auto_submit/763.partition-labels.py
@@ -4,9 +4,9 @@
 # https://leetcode.com/problems/partition-labels/description/
 #
 # algorithms
-# Medium (64.65%)
-# Total Accepted:    6.4K
-# Total Submissions: 10K
+# Medium (65.43%)
+# Total Accepted:    25.2K
+# Total Submissions: 38.6K
 # Testcase Example:  '"ababcbacadefegdehijhklij"'
 #
 # 
@@ -32,21 +32,26 @@
 # S will consist of lowercase letters ('a' to 'z') only.
 # 
 #
-from collections import Counter
 class Solution(object):
     def partitionLabels(self, S):
         """
         :type S: str
         :rtype: List[int]
         """
-        counter = Counter(S)
-        counting = Counter()
-        output = []
-        for i, c in enumerate(S):
-            counting[c] += 1
-            if all(counter[k] == v for k, v in counting.iteritems()):
-                output.append(i + 1 - sum(output))
-        return output
-
+        partitions = {}
+        for i, s in enumerate(S):
+            if s not in partitions:
+                partitions[s] = [i, i]
+            else:
+                partitions[s][1] = i
+        #print partitions
+        labels = []
+        for st, end in sorted(partitions.values()):
+            if labels and labels[-1][0] <= st <= labels[-1][1]:
+               labels[-1][1] = max(labels[-1][1], end)
+            else:
+                labels.append([st, end])
+        return [end - st + 1 for st, end in labels]
+            
     def test(self):
         print self.partitionLabels("ababcbacadefegdehijhklij")
