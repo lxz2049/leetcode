5c7da9cd7eadc5d606a719c333c81ae2216577ae
lxz2049 <lxz2049@gmail.com>
2018-10-13 20:21:16 -0700

    update 797.all-paths-from-source-to-target.py

---
+++ auto_submit/797.all-paths-from-source-to-target.py
@@ -0,0 +1,57 @@
+#
+# [813] All Paths From Source to Target
+#
+# https://leetcode.com/problems/all-paths-from-source-to-target/description/
+#
+# algorithms
+# Medium (67.14%)
+# Total Accepted:    14.7K
+# Total Submissions: 21.9K
+# Testcase Example:  '[[1,2],[3],[3],[]]'
+#
+# Given a directed, acyclic graph of N nodes.  Find all possible paths from
+# node 0 to node N-1, and return them in any order.
+# 
+# The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.
+# graph[i] is a list of all nodes j for which the edge (i, j) exists.
+# 
+# 
+# Example:
+# Input: [[1,2], [3], [3], []] 
+# Output: [[0,1,3],[0,2,3]] 
+# Explanation: The graph looks like this:
+# 0--->1
+# |    |
+# v    v
+# 2--->3
+# There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+# 
+# 
+# Note:
+# 
+# 
+# The number of nodes in the graph will be in the range [2, 15].
+# You can print different paths in any order, but you should keep the order of
+# nodes inside one path.
+# 
+# 
+#
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        target = len(graph)-1
+        ret = []
+        def traverse(node, path):
+            if node == target:
+                ret.append(path)
+            else:
+                for neighbor in graph[node]:
+                    traverse(neighbor, path + [neighbor])
+        traverse(0, [0])
+        return ret
+
+    def test(self):
+        print self.allPathsSourceTarget([[1,2],[3],[3],[]])
