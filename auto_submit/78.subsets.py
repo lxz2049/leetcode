854f3c459b20b04c68a019db3135a169533cea8a
lxz2049 <lxz2049@gmail.com>
2018-11-09 18:42:50 -0800

    update 78.subsets.py

---
+++ auto_submit/78.subsets.py
@@ -0,0 +1,43 @@
+#
+# [78] Subsets
+#
+# https://leetcode.com/problems/subsets/description/
+#
+# algorithms
+# Medium (48.03%)
+# Total Accepted:    292.2K
+# Total Submissions: 600.8K
+# Testcase Example:  '[1,2,3]'
+#
+# Given a set of distinct integers, nums, return all possible subsets (the
+# power set).
+# 
+# Note: The solution set must not contain duplicate subsets.
+# 
+# Example:
+# 
+# 
+# Input: nums = [1,2,3]
+# Output:
+# [
+# ⁠ [3],
+# [1],
+# [2],
+# [1,2,3],
+# [1,3],
+# [2,3],
+# [1,2],
+# []
+# ]
+# 
+#
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[nums[i] for i, b in enumerate(bin(n)[2:][::-1]) if b == "1"] for n in xrange(1<<len(nums))]
+
+    def test(self):
+        print self.subsets([1,2,3])
