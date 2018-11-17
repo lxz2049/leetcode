175145ab3b493ed907f30872872b5e5d606711df
lxz2049 <lxz2049@gmail.com>
2018-11-17 15:19:41 -0800

    update 67.add-binary.py

--- auto_submit/67.add-binary.py
+++ auto_submit/67.add-binary.py
@@ -4,9 +4,9 @@
 # https://leetcode.com/problems/add-binary/description/
 #
 # algorithms
-# Easy (34.53%)
-# Total Accepted:    224.8K
-# Total Submissions: 634.6K
+# Easy (36.36%)
+# Total Accepted:    247.8K
+# Total Submissions: 679.3K
 # Testcase Example:  '"11"\n"1"'
 #
 # Given two binary strings, return their sum (also a binary string).
@@ -26,7 +26,6 @@
 # Output: "10101"
 # 
 #
-import itertools
 class Solution(object):
     def addBinary(self, a, b):
         """
@@ -34,4 +33,17 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        return bin(int(a, 2) + int(b, 2))[2:]
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        ret = []
+        while i >= 0 or j >= 0:
+            ba = int(a[i]) if i >= 0 else 0
+            bb = int(b[j]) if j >= 0 else 0
+            ret.append((carry + ba + bb) % 2)
+            carry = (carry + ba + bb) / 2
+            i -= 1
+            j -= 1
+        if carry:
+            ret.append(1)
+        return "".join(str(b) for b in reversed(ret))
