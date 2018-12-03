505c9b3debc1bdaf13085ff7a077c97096e5212d
lxz2049 <lxz2049@gmail.com>
2018-12-02 22:30:44 -0800

    update 34.find-first-and-last-position-of-element-in-sorted-array.py

---
+++ auto_submit/34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,44 @@
+#
+# [34] Find First and Last Position of Element in Sorted Array
+#
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+#
+# algorithms
+# Medium (32.29%)
+# Total Accepted:    241.2K
+# Total Submissions: 745K
+# Testcase Example:  '[5,7,7,8,8,10]\n8'
+#
+# Given an array of integers nums sorted in ascending order, find the starting
+# and ending position of a given target value.
+# 
+# Your algorithm's runtime complexity must be in the order of O(log n).
+# 
+# If the target is not found in the array, return [-1, -1].
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+# 
+#
+import bisect
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lo = bisect.bisect_left(nums, target)
+        if lo < len(nums) and nums[lo] == target:
+            hi = next((i for i in xrange(lo+1, len(nums)) if nums[i] != target), len(nums))
+            return [lo, hi-1]
+        return [-1, -1]
