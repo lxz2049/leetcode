db384b91a5434a6e283e0ac826a2c0dcb929a87e
lxz2049 <lxz2049@gmail.com>
2018-12-11 17:49:56 -0800

    update 380.insert-delete-getrandom-o1.py

---
+++ auto_submit/380.insert-delete-getrandom-o1.py
@@ -0,0 +1,109 @@
+#
+# [380] Insert Delete GetRandom O(1)
+#
+# https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+#
+# algorithms
+# Medium (40.63%)
+# Total Accepted:    87.3K
+# Total Submissions: 211.6K
+# Testcase Example:  '["RandomizedSet","insert","remove","insert","getRandom","remove","insert","getRandom"]\n[[],[1],[2],[2],[],[1],[2],[]]'
+#
+# Design a data structure that supports all following operations in average
+# O(1) time.
+# 
+# 
+# 
+# insert(val): Inserts an item val to the set if not already present.
+# remove(val): Removes an item val from the set if present.
+# getRandom: Returns a random element from current set of elements. Each
+# element must have the same probability of being returned.
+# 
+# 
+# 
+# Example:
+# 
+# // Init an empty set.
+# RandomizedSet randomSet = new RandomizedSet();
+# 
+# // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+# randomSet.insert(1);
+# 
+# // Returns false as 2 does not exist in the set.
+# randomSet.remove(2);
+# 
+# // Inserts 2 to the set, returns true. Set now contains [1,2].
+# randomSet.insert(2);
+# 
+# // getRandom should return either 1 or 2 randomly.
+# randomSet.getRandom();
+# 
+# // Removes 1 from the set, returns true. Set now contains [2].
+# randomSet.remove(1);
+# 
+# // 2 was already in the set, so return false.
+# randomSet.insert(2);
+# 
+# // Since 2 is the only number in the set, getRandom always return 2.
+# randomSet.getRandom();
+# 
+# 
+#
+import random
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.set = set()
+        
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.set:
+            return False
+        self.set.add(val)
+        return True
+        
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.set:
+            return False
+        self.set.remove(val)
+        return True
+        
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return random.choice(list(self.set))
+        
+
+class Solution:
+    def test(self):
+        s = RandomizedSet()
+        print s.insert(1)
+        print s.remove(2)
+        print s.insert(2)
+        print s.getRandom()
+        print s.remove(1)
+        print s.insert(2)
+        print s.getRandom()
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
