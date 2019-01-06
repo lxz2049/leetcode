4ae9a62f8b0bce745040d64d7f447b481fbdfbc4
lxz2049 <lxz2049@gmail.com>
2019-01-05 17:15:35 -0800

    update 155.min-stack.py

---
+++ auto_submit/155.min-stack.py
@@ -0,0 +1,93 @@
+#
+# [155] Min Stack
+#
+# https://leetcode.com/problems/min-stack/description/
+#
+# algorithms
+# Easy (34.24%)
+# Total Accepted:    250.4K
+# Total Submissions: 721.2K
+# Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+#
+# 
+# Design a stack that supports push, pop, top, and retrieving the minimum
+# element in constant time.
+# 
+# 
+# push(x) -- Push element x onto stack.
+# 
+# 
+# pop() -- Removes the element on top of the stack.
+# 
+# 
+# top() -- Get the top element.
+# 
+# 
+# getMin() -- Retrieve the minimum element in the stack.
+# 
+# 
+# 
+# 
+# Example:
+# 
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin();   --> Returns -3.
+# minStack.pop();
+# minStack.top();      --> Returns 0.
+# minStack.getMin();   --> Returns -2.
+# 
+# 
+#
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minstack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        minx = x
+        if self.minstack:
+            minx = min(x, self.minstack[-1])
+        self.stack.append(x)
+        self.minstack.append(minx)
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+        self.minstack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minstack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
