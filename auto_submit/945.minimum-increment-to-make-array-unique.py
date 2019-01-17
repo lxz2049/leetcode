75f2967cf0bbc779256af9dfb685535b61b50b9b
lxz2049 <lxz2049@gmail.com>
2019-01-16 18:45:50 -0800

    update 945.minimum-increment-to-make-array-unique.py

---
+++ auto_submit/945.minimum-increment-to-make-array-unique.py
@@ -0,0 +1,70 @@
+#
+# [982] Minimum Increment to Make Array Unique
+#
+# https://leetcode.com/problems/minimum-increment-to-make-array-unique/description/
+#
+# algorithms
+# Medium (40.56%)
+# Total Accepted:    6.7K
+# Total Submissions: 16.3K
+# Testcase Example:  '[1,2,2]'
+#
+# Given an array of integers A, a move consists of choosing any A[i], and
+# incrementing it by 1.
+# 
+# Return the least number of moves to make every value in A unique.
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: [1,2,2]
+# Output: 1
+# Explanation:  After 1 move, the array could be [1, 2, 3].
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [3,2,1,2,1,7]
+# Output: 6
+# Explanation:  After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
+# It can be shown with 5 or less moves that it is impossible for the array to
+# have all unique values.
+# 
+# 
+# 
+# 
+# 
+# Note:
+# 
+# 
+# 0 <= A.length <= 40000
+# 0 <= A[i] < 40000
+# 
+# 
+# 
+# 
+# 
+#
+class Solution(object):
+    def minIncrementForUnique(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        ret = 0
+        cur = -1
+        for n in sorted(A):
+            if n <= cur:
+                cur += 1
+            else:
+                cur = n
+            ret += cur - n
+        return ret
+
+    def test(self):
+        print self.minIncrementForUnique([1,2,2])
+        print self.minIncrementForUnique([3,2,1,2,1,7])
