427950db4e1be844a3eedbee461ca3610bf3f519
lxz2049 <lxz2049@gmail.com>
2019-01-20 16:10:52 -0800

    update 46.permutations.py

---
+++ auto_submit/46.permutations.py
@@ -0,0 +1,40 @@
+#
+# [46] Permutations
+#
+# https://leetcode.com/problems/permutations/description/
+#
+# algorithms
+# Medium (51.58%)
+# Total Accepted:    310.1K
+# Total Submissions: 598.8K
+# Testcase Example:  '[1,2,3]'
+#
+# Given a collection of distinct integers, return all possible permutations.
+# 
+# Example:
+# 
+# 
+# Input: [1,2,3]
+# Output:
+# [
+# ⁠ [1,2,3],
+# ⁠ [1,3,2],
+# ⁠ [2,1,3],
+# ⁠ [2,3,1],
+# ⁠ [3,1,2],
+# ⁠ [3,2,1]
+# ]
+# 
+# 
+#
+from itertools import permutations
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(permutations(nums))
+
+    def test(self):
+        print self.permute([1,2,3])
